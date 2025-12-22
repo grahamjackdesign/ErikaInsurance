f6e2397903460e68ae6c41e2afcb2e754bbd1ec1
jack <grahamjackdesign@gmail.com>
2025-12-22 16:44:53 -0600

    favicons

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,6 +34,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="es">
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="/favicon-16x16.png" type="image/png" sizes="16x16" />
+        <link rel="icon" href="/favicon-32x32.png" type="image/png" sizes="32x32" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+      </head>
       <body className={`${roboto.className} ${poppins.variable}`}>{children}</body>
     </html>
   )
